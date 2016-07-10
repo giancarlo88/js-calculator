d94c5e29d5f666dada36e02e5eaabcdefdd4e4b9
Giancarlo <galliani.08@gmail.com>
2016-07-10 21:42:01 +0100

    Updates
    Minor updates: 
    -Fixed issue with decimals not working for numbers less than 10. 
    -Changed CSS so buttons are aligned better.
    -Added check to ensure answers can't go past the width of the "display" (this needs more work)

--- index.js
+++ index.js
@@ -1,62 +1,63 @@
 var storage = 0;
-var input = 0
-var lastOp = ""
-var lastButton = ""
+var input = 0;
+var lastOp = "";
+var lastButton = "";
 
-$(".operator").on("click", function(){
-  input = parseFloat($("#nums").val(), 10);
-  console.log(lastOp);
-  console.log(input);
-  console.log(storage);
-  switch (true){
-    case (lastOp == "+"):
-    storage += input;
-      break;
-    case (lastOp == "-"):
-      storage -= input;
-      break;
-    case (lastOp == "*"):
-      storage *= input; 
-      break;
-    case (lastOp == "/"):
-      storage /= input; 
-      break;
+$(".operator").on("click", function () {
+    input = parseFloat($("#nums").val(), 10);
+    switch (true) {
+    case (lastOp === "+"):
+        storage += input;
+        break;
+    case (lastOp === "-"):
+        storage -= input;
+        break;
+    case (lastOp === "*"):
+        storage *= input;
+        break;
+    case (lastOp === "/"):
+        storage /= input;
+        break;
     default:
-      storage = input;
-      break;
-  }
-  lastOp = $(this).attr("id");
-  lastButton = lastOp
-  $("#nums").val(storage);
-  console.log(lastOp);
-})
+        storage = input;
+        break;
+    }
+    lastOp = $(this).attr("id");
+    lastButton = lastOp;
+    if (storage >= 9999999999) {
+        $('#nums').val("E");
+    }
+    else {
+        $('#nums').val(storage);
+    }
+});
 
-$(".number").on("click", function(){
-if (lastButton != "number" ){
-$("#nums").val("")
-};
-$("#nums").val($("#nums").val()+$(this).attr("id"))
-lastButton = "number";
-})
 
-$(".dec").on("click", function(){
-if (lastButton != "number" ){
-$("#nums").val("");
-};
-console.log($("#nums").val().length)  
-if (($("#nums").val().length) < 2){
-$("#nums").val("0"+$(this).attr("id"))
-} else {
-$("#nums").val($("#nums").val()+$(this).attr("id"))}
-lastButton = "number";
-})
+$(".number").on("click", function () {
+    if (lastButton !== "number") {
+        $("#nums").val("");
+    }
+    $("#nums").val($("#nums").val() + $(this).attr("id"));
+    lastButton = "number";
+});
 
-$("#clear").on("click", function(){
-  input = 0;
-  storage = 0; 
-  lastOp = "";
-  $("#nums").val(0);
-  console.log(input);
-  console.log(storage);
-  lastButton = "clear"
-})
+$(".dec").on("click", function () {
+    if (lastButton !== "number") {
+        $("#nums").val("");
+    }
+    if (($("#nums").val().length) < 1) {
+        $("#nums").val("0" + $(this).attr("id"));
+    }
+    else {
+        $("#nums").val($("#nums").val() + $(this).attr("id"));
+        }
+        lastButton = "number";
+});
+
+$("#clear").on("click", function () {
+    input = 0;
+    storage = 0;
+    lastOp = "";
+    $("#nums").val(0);
+    lastButton = "clear";
+});
